7da121dab5073f5246f2896042ce5b12eb4d06bb
Xiaochen Wang <xwang345@myseneca.ca>
2018-05-11 14:34:08 -0400

    first commit

---
+++ lab1.java
@@ -0,0 +1,15 @@
+package test1;
+import java.util.Scanner;
+
+public class test1 {
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter an integer value: ");
+		int value =input.nextInt();
+		
+		System.out.println("You have enter the value: " +value);
+		System.out.println("Welcome to JAC444 Lab1");
+	}
+	
+
+}
